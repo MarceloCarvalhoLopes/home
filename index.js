2be232db4400e61b07225cce92d67ffd17f57038
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2019-04-25 14:02:20 -0300

    now.json, views/contato e curriculum

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname,'public')))
 app.set('views', path.join(__dirname,'views'))
@@ -11,7 +13,23 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000,(err) =>{
+app.get('/curriculum', (req,res) => {
+    res.render('curriculum',{
+    })
+})
+
+app.get('/home', (req,res) => {
+    res.render('home', {
+    })
+})
+
+app.get('/contato', (req, res) => {
+    res.render('contato', {
+    })
+})
+
+
+app.listen(port,(err) =>{
     if (err){
         console.log('Não foi possível iniciar o servidor')
     }else{
